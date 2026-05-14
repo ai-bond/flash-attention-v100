c79cedd8ce54683abb3f375c3d5861d87d0f155b
James Bond <jamesbond_007@my.com>
2026-05-14 16:59:42 +0000

    Interface: Add kvcache func export to attention packet

--- flash_attn/flash_attn_interface.py
+++ flash_attn/flash_attn_interface.py
@@ -2,12 +2,16 @@ from flash_attn_v100.flash_attn_interface import (
     flash_attn_func,
     flash_attn_gpu,
     flash_attn_varlen_func,
-    flash_attn_varlen_gpu
+    flash_attn_varlen_gpu,
+    flash_attn_with_kvcache,
+    flash_attn_with_kvcache_gpu
 )
 
 __all__ = [
     "flash_attn_func",
     "flash_attn_gpu",
     "flash_attn_varlen_func",
-    "flash_attn_varlen_gpu"
+    "flash_attn_varlen_gpu",
+    "flash_attn_with_kvcache",
+    "flash_attn_with_kvcache_gpu",
 ]
